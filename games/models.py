145154163e6fa208b4cd5957ddf2557f5260f78d
PaverErsh <pavel204812@gmail.com>
2022-01-22 19:33:42 +0200

    add urls for games and start page

---
+++ games/models.py
@@ -0,0 +1,16 @@
+from turtle import update
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created = models.DateTimeField(auto_created=True)
+    update = models.DateTimeField(auto_created=True)
+
+
+
